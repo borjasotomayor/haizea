ec2f0edae1f5827879914ffa9c3a8f93b958bbde
Borja Sotomayor <borja@cs.uchicago.edu>
2006-09-28 01:34:05 +0000

    Graphical representation of traces

--- python/workspace/graphing/graph.py
+++ python/workspace/graphing/graph.py
@@ -3,69 +3,136 @@ import matplotlib
 import pylab
 from pylab import array, arange
 
+class Graph(object):
+    def __init__(self):
+        pass
+    
+    def plot(self):
+        pass
+    
+    def show(self):
+        pass
+
+
+#TODO: Factor out stuff like labels, etc.
 class ScheduleGraph(object):
-    def __init__(self, data, legends=[], firstempty=False):
+    def __init__(self, data, legends=[], subdata=[], sublegends=[], firstempty=False):
         self.data = data
         self.legends = legends
         self.firstempty = firstempty
-        self.patches = []
+        self.subdata = subdata
+        self.sublegends = sublegends
     
     def plot(self):
-        pylab.axes([0.1,0.1,0.7,0.8])
+        #pylab.axes([0.1,0.1,0.65,0.8])
         numTracks = len(self.data[0])
         numDivisions = len(self.data)
-        yLabels = ['%d' % (x+1) for x in range(len(data[0]))]
+        yLabels = ['%d' % (x+1) for x in range(len(self.data[0]))]
         xLabels = [] # Not used
+        
         
         #TODO: Check for empty legends
         colors = map(lambda x: x.color,self.legends)
+        #subcolors = filter(lambda x: x!=None, self.sublegends)
+        subcolors = map(lambda x: x.color, self.sublegends)
         
         #TODO: Use constants
-        width = 0.4     # the width of the bars
-        ind = arange(numTracks) * (width *1.35)  # the y locations for the tracks
+        width = 0.6     # the width of the bars
+        ind = arange(numTracks) * (width *1.5)  # the y locations for the tracks
+        print ind
         if self.firstempty:
-            xoff= array(data[0])
+            xoff= array(self.data[0])
             divs = xrange(numDivisions-1)
         else:
             xoff = array([0.0] * numTracks) # the left values for stacked bar chart
             divs = xrange(numDivisions)
+        
+        patches = []
+        subrectangles = []
 
+        #TODO: Replace with "for row in self.data:"?
         for div in divs:
             if self.firstempty: 
                 row = div + 1
             else:
                 row = div
-            rectangles = pylab.barh(data[row], ind, width, left=xoff, color=colors[row])
-            self.patches.append(rectangles[0])
-            #barh(data[row+1], ind, width-0.2, left=yoff, color=colours[row])
-            xoff = xoff + data[row]
+            print self.data[row]
+            print colors[row]
+            rectangles = pylab.barh(self.data[row], ind, width, left=xoff, color=colors[row])
+            patches.append(rectangles[0])
+            if len(self.subdata)>0 and len(self.subdata[row]) > 0:
+                rectangles = pylab.barh(self.subdata[row], ind, width-0.3, left=xoff, color=subcolors[row])
+                subrectangles.append(rectangles[0])
+            xoff = xoff + self.data[row]
     
+        patches += subrectangles
+        
         pylab.ylabel("Virtual Workspace")
         pylab.xlabel("Time (s)")
         accum = array([0.0]*numTracks)
-        for row in data:
+        for row in self.data:
             accum += array(row)
-        vals = arange(0, max(accum), 500000)
+        vals = arange(0, max(accum), 500)
         pylab.xticks(vals, ['%d' % val for val in vals])
         pylab.yticks(ind, yLabels)
         pylab.title('Trace')
 
         if self.firstempty:
-            legendText = map(lambda x: x.name,self.legends[1:])
+            legendText = map(lambda x: x.name,self.legends[1:]) + map(lambda x: x.name,self.sublegends[1:])
         else:
-            legendText = map(lambda x: x.name,self.legends)
+            legendText = map(lambda x: x.name,self.legends) #+ map(lambda x: x.name,self.sublegends)
 
-        pylab.legend(self.patches, legendText, (1.05,0.5))
+        legendText = filter(lambda x: x!=None, legendText)
+
+        pylab.legend(patches, legendText, (0.5,0.1))
 
     
     def show(self):
         pylab.show()
         
+class PointGraph(Graph):
+    def __init__(self, data, legends=[]):
+        self.data = data
+        self.legends = legends
+
+    def plot(self):
+        for dataset in self.data:
+            x = map(lambda x: x[0], dataset)
+            y = map(lambda x: x[1], dataset)
+            pylab.plot(x,y)
+    
+    def show(self):
+        pylab.show()
         
 class Legend(object):
-    def __init__(self, name, color):
+    def __init__(self, name=None, color=None):
         self.name = name
         self.color = color
+
+
+class Figure(object):
+    def __init__(self, graphs=[]):
+        self.graphs=graphs
+        
+    def addGraph(self, graph):
+        self.graphs.append(graph)
+        
+    def plot(self):
+
+        numGraphs=len(self.graphs)
+        axes = None
+        for i,graph in enumerate(self.graphs):
+            #TODO: This requires the funky 3-digit format. 
+            #Using rows,col,num doesn't work. Maybe this has
+            #been fixed in latest version?
+            #TODO: Currently, default is to share x axis. This
+            #should be configurable
+            axes = pylab.subplot((numGraphs*100)+10+i+1, sharex=axes)
+            graph.plot()
+            
+    def show(self):
+        pylab.show()
+        
 
 if __name__ == "__main__":    
     data = [[  66386,  174296,   75131,  577908,   32015,  174296,   75131,  577908,   32015,  174296,   75131,  577908,   32015,  174296,   75131,  577908,   32015],
@@ -80,7 +147,11 @@ if __name__ == "__main__":
                Legend("razzle","c"), 
                Legend("dazzle","m")]
     
-    g = ScheduleGraph(data,legends, firstempty=True)
-    g.plot()
-    g.show()
+    fig = Figure()
+    sched = ScheduleGraph(data,legends, firstempty=True)
+    fig.addGraph(sched)
+    sched.show()
+    #fig.show()
+    
 
+        
--- python/workspace/traces/files.py
+++ python/workspace/traces/files.py
@@ -1,4 +1,5 @@
 import sys
+from workspace.graphing.graph import ScheduleGraph, PointGraph, Legend, Figure
 
 class TraceEntry(object):
     pos = {
@@ -49,12 +50,52 @@ class TraceFile(object):
     def toFile(self,file):
         for entry in self.entries:
             print >>file, entry.toLine()
+            
+    def toGraph(self, bandwidth=1.0):
+        #TODO: Hardcoding is bad
+        data = [[],[],[]]
+        subdata = [[],[],[]]        
+        for entry in self.entries:
+            startTime = int(entry.fields["time"])
+            timeToDeadline = int(entry.fields["deadline"])
+            duration = int(entry.fields["duration"])
+            imageSize=int(entry.fields["size"])
+            data[0].append(startTime)
+            data[1].append(timeToDeadline)
+            data[2].append(duration)
+            subdata[1].append(imageSize/bandwidth)
+            
+        legends = [ Legend("Not submitted","gray"), 
+           Legend("Staging","orange"), 
+           Legend("Running","lightgreen")]
+
+        sublegends = [ Legend(), 
+           Legend("Transfer time (%dMb/s)" % (bandwidth*8),"red"), 
+           Legend()]
+        
+        graph = ScheduleGraph(data, legends, subdata, sublegends, firstempty=True)
+        return graph
+        
         
 if __name__ == "__main__":
     print "I've been run!"
     trace = TraceFile.fromFile("example.trace")
-    trace.toFile(sys.stdout)
+    #trace.toFile(sys.stdout)
+    fig = Figure()
+    graph = trace.toGraph()
+    fig.addGraph(graph)
     
+    data = [[(0,10),(1000,5),(2000,3),(3000,8),(4000,1),(5000,5)],
+            [(500,4),(1500,1),(2500,7),(3500,2),(4500,7),(4800,3)]]
+    
+    legends = [ Legend("foobar","r"), 
+               Legend("barfoo","g")]
+    
+    graph2 = PointGraph(data,legends)
+    fig.addGraph(graph2)
+    
+    fig.plot()
+    fig.show()
     
     
     
