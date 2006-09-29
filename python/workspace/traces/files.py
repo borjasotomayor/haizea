6533eb77cf00698107fb08935937ec0c8f279908
Borja Sotomayor <borja@cs.uchicago.edu>
2006-09-29 00:36:58 +0000

    - trace -> Image ID histogram - trace -> Image duration histogram

--- python/workspace/traces/files.py
+++ python/workspace/traces/files.py
@@ -1,5 +1,5 @@
 import sys
-from workspace.graphing.graph import ScheduleGraph, PointGraph, Legend, Figure
+from workspace.graphing.graph import *
 
 class TraceEntry(object):
     pos = {
@@ -51,7 +51,7 @@ class TraceFile(object):
         for entry in self.entries:
             print >>file, entry.toLine()
             
-    def toGraph(self, bandwidth=1.0):
+    def toScheduleGraph(self, bandwidth=1.0):
         #TODO: Hardcoding is bad
         data = [[],[],[]]
         subdata = [[],[],[]]        
@@ -75,6 +75,22 @@ class TraceFile(object):
         
         graph = ScheduleGraph(data, legends, subdata, sublegends, firstempty=True)
         return graph
+
+    def toImageHistogram(self):
+        data = []
+        for entry in self.entries:
+            imageName=entry.fields["uri"].split("/").pop()
+            data.append(imageName)
+        graph = DiscreteHistogram(data,"Images","# of images", normalized=True)
+        return graph
+        
+    def toDurationHistogram(self):
+        data = []
+        for entry in self.entries:
+            duration = float(entry.fields["duration"])
+            data.append(duration)
+        graph = ContinuousHistogram(data,"Duration","# of images",normalized=True)
+        return graph   
         
         
 if __name__ == "__main__":
@@ -82,8 +98,9 @@ if __name__ == "__main__":
     trace = TraceFile.fromFile("example.trace")
     #trace.toFile(sys.stdout)
     fig = Figure()
-    graph = trace.toGraph()
-    fig.addGraph(graph)
+    graph = trace.toScheduleGraph()
+    imageGraph = trace.toImageHistogram()
+    durGraph = trace.toDurationHistogram()
     
     data = [[(0,10),(1000,5),(2000,3),(3000,8),(4000,1),(5000,5)],
             [(500,4),(1500,1),(2500,7),(3500,2),(4500,7),(4800,3)]]
@@ -92,7 +109,11 @@ if __name__ == "__main__":
                Legend("barfoo","g")]
     
     graph2 = PointGraph(data,legends)
-    fig.addGraph(graph2)
+    fig.addGraph(imageGraph,3,2,1)
+    fig.addGraph(durGraph,3,2,2)
+    fig.addGraph(graph,3,1,2)
+    fig.addGraph(graph2,3,1,3, sharex=graph)
+    
     
     fig.plot()
     fig.show()
