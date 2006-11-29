d2f94a89aad5f4047a01d6a8c5e825bb9f2e8d2c
Borja Sotomayor <borja@cs.uchicago.edu>
2006-11-29 21:56:54 +0000

    Implemented EARS (sans slot fitting yet) with simulated time, and ability to run through traces.

--- python/workspace/ears/db.py
+++ python/workspace/ears/db.py
@@ -8,7 +8,7 @@ class ReservationDB(object):
     def printReservations(self):
         conn = self.getConn()
         cur = conn.cursor()
-        cur.execute("SELECT * FROM TB_ALLOC")
+        cur.execute("SELECT * FROM V_ALLOCATION")
         
         for row in cur:
             print row
@@ -17,7 +17,7 @@ class ReservationDB(object):
         return None
     
     def existsRemainingReservations(self, afterTime):
-        sql = "SELECT COUNT(*) FROM tb_alloc WHERE all_schedend > ?"
+        sql = "SELECT COUNT(*) FROM tb_alloc WHERE all_schedend >= ?"
         cur = self.getConn().cursor()
         cur.execute(sql, (afterTime,))
         count = cur.fetchone()[0]
@@ -27,17 +27,44 @@ class ReservationDB(object):
         else:
             return True
 
-    def getAllocationsInInterval(self, time, td, status=None):
-        sql = """SELECT * FROM v_allocation WHERE 
-        all_schedstart >= ? and all_schedstart < ?"""
+    def getAllocationsInInterval(self, time, td, eventfield, distinct=None, allocstatus=None, res=None):
+        if distinct==None:
+            sql = "SELECT * FROM v_allocation"
+        else:
+            sql = "SELECT DISTINCT"
+            for field in distinct:
+                sql += " %s," % field
+            sql=sql[:-1]
+            sql += " FROM v_allocation" 
+                
+        sql += " WHERE %s >= ? AND %s < ?" % (eventfield, eventfield)        
 
-        if status != None:
-            sql += " and all_status = %i" % status
-        
+        if allocstatus != None:
+            sql += " AND all_status = %i" % allocstatus
+
+        if res != None:
+            sql += " AND res_id = %i" % res
+
         cur = self.getConn().cursor()
         cur.execute(sql, (time, time+td))
         
-        return cur
+        return cur        
+
+    def getReservationsWithStartingAllocationsInInterval(self, time, td, **kwargs):
+        distinctfields=("RES_ID","RES_NAME","RES_STATUS")
+        return self.getAllocationsInInterval(time,td,"all_schedstart", distinct=distinctfields, **kwargs)
+
+    def getResPartsWithStartingAllocationsInInterval(self, time, td, **kwargs):
+        distinctfields=("RSP_ID","RSP_NAME","RSP_STATUS")
+        return self.getAllocationsInInterval(time,td,"all_schedstart", distinct=distinctfields, **kwargs)
+
+    def getReservationsWithEndingAllocationsInInterval(self, time, td, **kwargs):
+        distinctfields=("RES_ID","RES_NAME","RES_STATUS")
+        return self.getAllocationsInInterval(time,td,"all_schedend", distinct=distinctfields, **kwargs)
+
+    def getResPartsWithEndingAllocationsInInterval(self, time, td, **kwargs):
+        distinctfields=("RSP_ID","RSP_NAME","RSP_STATUS")
+        return self.getAllocationsInInterval(time,td,"all_schedend", distinct=distinctfields, **kwargs)
     
     def updateReservationStatus(self, res_id, status):
         sql = "UPDATE TB_RESERVATION SET RES_STATUS = ? WHERE RES_ID = ?"
@@ -49,6 +76,33 @@ class ReservationDB(object):
         sql = "UPDATE TB_RESPART SET RSP_STATUS = ? WHERE RSP_ID = ?"
         cur = self.getConn().cursor()
         cur.execute(sql, (status,respart_id))
+
+    def updateAllocationStatusInInterval(self, status, respart=None, start=None, end=None):
+        sql = "UPDATE TB_ALLOC SET ALL_STATUS = ? WHERE "
+        if respart != None:
+            sql += " RSP_ID=%i" % respart
+            
+        if start != None:
+            sql += " AND all_schedstart >= ? AND all_schedstart < ?"
+            interval = start
+        elif end != None:
+            sql += " AND all_schedend >= ? AND all_schedend < ?"
+            interval = end
+        
+        cur = self.getConn().cursor()
+        cur.execute(sql, (status,) + interval)
+
+    def isReservationDone(self, res_id):
+        sql = "SELECT COUNT(*) FROM V_ALLOCATION WHERE res_id=? AND all_status in (0,1)" # Hardcoding bad!
+        cur = self.getConn().cursor()
+        cur.execute(sql, (res_id,))
+        
+        count = cur.fetchone()[0]
+
+        if count == 0:
+            return True
+        else:
+            return False
 
     def commit(self):
         self.getConn().commit()
--- python/workspace/ears/server.py
+++ python/workspace/ears/server.py
@@ -1,4 +1,5 @@
 from workspace.ears import db
+from workspace.traces.files import TraceFile
 from datetime import datetime, timedelta
 
 STATUS_PENDING = 0
@@ -6,64 +7,150 @@ STATUS_RUNNING = 1
 STATUS_DONE = 2
 
 class BaseServer(object):
-    def __init__(self, resDB):
+    def __init__(self, resDB, trace):
         self.resDB = resDB
+        self.trace = trace
         
     def processReservations(self, time, td):
         # Check for reservations which must start
-        allocs = self.resDB.getAllocationsInInterval(time, td, status=STATUS_PENDING)
+        rescur = self.resDB.getReservationsWithStartingAllocationsInInterval(time, td, allocstatus=STATUS_PENDING)
+        reservations = rescur.fetchall()
         some = False
-        allocs2 = allocs.fetchall()
-        for alloc in allocs2:
+        for res in reservations:
             some = True
-            # Check to see if this is the start of a reservation
-            if alloc["RES_STATUS"] == STATUS_PENDING:
-                self.startReservation(alloc["RES_ID"], cursor=alloc)
-            # Check to see if this is the start of a reservation part    
-            if alloc["RSP_STATUS"] == STATUS_PENDING:    
-                # The reservation part hasn't started yet
-                self.startReservationPart(alloc["RSP_ID"], cursor=alloc)
-            elif alloc["RSP_STATUS"] == STATUS_RUNNING:
-                # The reservation part has already started
-                # This is a change in resource allocation
-                self.resizeAllocation(alloc)
+             # Check to see if this is the start of a reservation
+            if res["RES_STATUS"] == STATUS_PENDING:
+                self.startReservation(res["RES_ID"], row=res)
+            elif res["RES_STATUS"] == STATUS_RUNNING:
+                pass # do nothing
+            
+            # Get reservation parts that have to start
+            rspcur = self.resDB.getResPartsWithStartingAllocationsInInterval(time,td, allocstatus=STATUS_PENDING, res=res["RES_ID"])
+            resparts = rspcur.fetchall()
+            
+            for respart in resparts:
+                if respart["RSP_STATUS"] == STATUS_PENDING:    
+                    # The reservation part hasn't started yet
+                    self.startReservationPart(respart["RSP_ID"], row=respart, resname=res["RES_NAME"])
+                    self.startAllocations(respart["RSP_ID"], time, td)
+                elif respart["RSP_STATUS"] == STATUS_RUNNING:
+                    # The reservation part has already started
+                    # This is a change in resource allocation
+                    self.resizeAllocation(respart["RSP_ID"], row=respart)                
             
         if some: print ""
-        # Check for reservations which must end
-        allocs = self.resDB.getAllocationsInInterval(time, td, status=STATUS_RUNNING)
+        
+        rescur = self.resDB.getReservationsWithEndingAllocationsInInterval(time, td, allocstatus=STATUS_RUNNING)
+        reservations = rescur.fetchall()
+        some = False
+        for res in reservations:
+            some = True
+          
+            # Get reservation parts that have to end
+            rspcur = self.resDB.getResPartsWithEndingAllocationsInInterval(time,td, allocstatus=STATUS_RUNNING, res=res["RES_ID"])
+            resparts = rspcur.fetchall()
+            
+            for respart in resparts:
+                if respart["RSP_STATUS"] == STATUS_RUNNING:
+                    # An allocation in this reservation part is ending.
+                    # Is it the last one?
+                    if self.isReservationPartEnd(respart["RSP_ID"], time, td):
+                        self.stopReservationPart(respart["RSP_ID"], row=respart, resname=res["RES_NAME"])
+                        self.stopAllocations(respart["RSP_ID"], time, td)
+                    else:
+                        print "Resource allocation resizing not supported yet"
+
+             # Check to see if this ends the reservation
+            if self.isReservationDone(res["RES_ID"]):
+                self.stopReservation(res["RES_ID"], row=res)
+        if some: print ""
         
         #self.resDB.commit()
 
-    def startReservation(self, res_id, cursor=None):
-        self.resDB.updateReservationStatus(res_id, STATUS_RUNNING)
-        if cursor != None:
-            print "%s: Starting reservation '%s'" % (self.getTime(), cursor["RES_NAME"])
+    def processTraceRequests(self, delta):
+        seconds = delta.seconds
+        reqToProcess = [r for r in self.trace.entries if int(r.fields["time"]) <= seconds]
+        newtrace = [r for r in self.trace.entries if int(r.fields["time"]) > seconds]
         
+        for r in reqToProcess:
+            reqTime = self.getTime() #Should starttime + seconds
+            startTime = reqTime + timedelta(seconds=int(r.fields["deadline"]))
+            endTime = startTime + timedelta(seconds=int(r.fields["duration"]))
+            print "%s: Received request for VW" % reqTime
+            print "\tStart time: %s" % startTime
+            print "\tEnd time: %s" % endTime
+            print "\tNodes: %i" % int(r.fields["numNodes"])
+            
+            self.processRequest(startTime, endTime)
+        
+        self.trace.entries = newtrace
+        
+    def processRequest(self, startTime, endTime, mem=512.0, cpu=25.0, imguri, numnodes):
+        # Estimate image transfer time
+        imgTransferTime = timedelta(minutes=15)
+        
+        # Let the slot fitting begin!
+        
+        # Is image transfer feasible?
+
+
+    def startReservation(self, res_id, row=None):
+        if row != None:
+            print "%s: Starting reservation '%s'" % (self.getTime(), row["RES_NAME"])
+        self.resDB.updateReservationStatus(res_id, STATUS_RUNNING)
+
+    def stopReservation(self, res_id, row=None):
+        if row != None:
+            print "%s: Stopping reservation '%s'" % (self.getTime(), row["RES_NAME"])
+        self.resDB.updateReservationStatus(res_id, STATUS_DONE)
     
-    def startReservationPart(self, respart_id, cursor=None):
-        if cursor != None:
-            print "%s: Starting reservation part '%s'" % (self.getTime(), cursor["RSP_NAME"])
+    def startReservationPart(self, respart_id, row=None, resname=None):
+        if row != None:
+            print "%s: Starting reservation part '%s' of reservation %s" % (self.getTime(), row["RSP_NAME"], resname)
         self.resDB.updateReservationPartStatus(respart_id, STATUS_RUNNING)
 
-    def resizeAllocation(self, res_id, cursor=None):
+    def startAllocations(self, respart_id, time, td):
+        self.resDB.updateAllocationStatusInInterval(STATUS_RUNNING, respart=respart_id, start=(time,time+td))
+
+    def stopAllocations(self, respart_id, time, td):
+        self.resDB.updateAllocationStatusInInterval(STATUS_DONE, respart=respart_id, end=(time,time+td))
+
+    def stopReservationPart(self, respart_id, row=None, resname=None):
+        if row != None:
+            print "%s: Stopping reservation part '%s' of reservation %s" % (self.getTime(), row["RSP_NAME"], resname)
+        self.resDB.updateReservationPartStatus(respart_id, STATUS_DONE)
+
+    def isReservationDone(self, res_id):
+        return self.resDB.isReservationDone(res_id)
+
+    def isReservationPartEnd(self, respart_id, time, td):
+        # We don't support changing allocation sizes yet
+        return True
+
+    def resizeAllocation(self, respart_id, row=None):
         print "An allocation has to be resized here. This should not be happening!"
     
 
 class SimulatingServer(BaseServer):
-    def __init__(self, resDB):
-        BaseServer.__init__(self, resDB)
+    def __init__(self, resDB, trace):
+        BaseServer.__init__(self, resDB, trace)
         self.time = None
         
     def start(self, forceStartTime=None):
         if forceStartTime == None:
-            self.time = datetime.now()
+            self.time = startTime = datetime.now()
         else:
-            self.time = forceStartTime
+            self.time = startTime = forceStartTime
+            
         td = timedelta(minutes=1)
+        
         while self.resDB.existsRemainingReservations(self.time):
+            if len(self.trace.entries) > 0:
+                delta = self.time - startTime
+                self.processTraceRequests(delta)
             self.processReservations(self.time, td)
             self.time = self.time + td
-            
+
         print "done"
 
     def getTime(self):
@@ -82,6 +169,9 @@ class RealServer(BaseServer):
 if __name__ == "__main__":
     dbfile = "/home/borja/files/db/reservations.db"
     resDB = db.SQLiteReservationDB(dbfile)
-    s = SimulatingServer(resDB)
+    
+    trace = TraceFile.fromFile("test.trace")
+    
+    s = SimulatingServer(resDB, trace)
     startTime = datetime(2006, 11, 25, 13, 00, 00) 
     s.start(forceStartTime=startTime)
