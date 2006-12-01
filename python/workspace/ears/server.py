cd55239c77b5e1612ec9b597450a975d3146db90
Borja Sotomayor <borja@cs.uchicago.edu>
2006-12-01 01:11:24 +0000

    Slot fitting

--- python/workspace/ears/server.py
+++ python/workspace/ears/server.py
@@ -1,10 +1,14 @@
 from workspace.ears import db
 from workspace.traces.files import TraceFile
 from datetime import datetime, timedelta
+from sets import Set
 
 STATUS_PENDING = 0
 STATUS_RUNNING = 1
 STATUS_DONE = 2
+
+SLOTTYPE_CPU=1
+SLOTTYPE_MEM=2
 
 class BaseServer(object):
     def __init__(self, resDB, trace):
@@ -76,20 +80,146 @@ class BaseServer(object):
             reqTime = self.getTime() #Should starttime + seconds
             startTime = reqTime + timedelta(seconds=int(r.fields["deadline"]))
             endTime = startTime + timedelta(seconds=int(r.fields["duration"]))
+            numNodes = int(r.fields["numNodes"])
             print "%s: Received request for VW" % reqTime
             print "\tStart time: %s" % startTime
             print "\tEnd time: %s" % endTime
-            print "\tNodes: %i" % int(r.fields["numNodes"])
+            print "\tNodes: %i" % numNodes
             
-            self.processRequest(startTime, endTime)
+            resources = {SLOTTYPE_CPU: 25.0, SLOTTYPE_MEM: 256.0}
+            
+            self.processRequest(startTime, endTime, None, numnodes=numNodes, resources=resources)
         
         self.trace.entries = newtrace
         
-    def processRequest(self, startTime, endTime, mem=512.0, cpu=25.0, imguri, numnodes):
-        # Estimate image transfer time
-        imgTransferTime = timedelta(minutes=15)
+    def processRequest(self, startTime, endTime, imguri, numnodes, resources):
+        # Estimate image transfer time 
+        imgTransferTime = timedelta(minutes=15) # We assume 15 minutes for tests
+        
+        slottypes = [SLOTTYPE_CPU, SLOTTYPE_MEM]
         
         # Let the slot fitting begin!
+        # First, find candidate slots
+        candidateslots = {}
+        
+        # TODO: There are multiple points at which the slot fitting could be aborted
+        # for lack of resources. Currently, this is only checked at the very end.
+        for slottype in slottypes:
+            candidateslots[slottype] = []
+            needed = resources[slottype]
+            slots = []
+            capacity={}
+            
+            # First sieve: select all slots that have enough resources at the beginning
+            slots1 = self.resDB.findAvailableSlots(time=startTime, amount=needed, type=slottype)
+            for slot in slots1:
+                slot_id = slot["SL_ID"]
+                slots.append(slot_id)
+                if not capacity.has_key(slot_id):
+                    capacity[slot_id] = []
+                capacity[slot_id].append((startTime,slot["available"]))
+
+            # Second sieve: remove slots that don't have enough resources at the end
+            slots2 = self.resDB.findAvailableSlots(time=endTime, amount=needed, slots=slots, closed=False)
+            slots = []
+            for slot in slots2:
+                nod_id = slot["NOD_ID"]
+                slot_id = slot["SL_ID"]
+                slots.append((nod_id,slot_id))
+                if not capacity.has_key(slot_id):
+                    capacity[slot_id] = []
+                capacity[slot_id].append((endTime,slot["available"]))
+
+            # Final sieve: Determine "resource change points" and make sure that 
+            # there is enough resources at each point too (and determine maximum
+            # available)
+            for slot in slots:
+                slot_id=slot[1]
+                changepoints = self.resDB.findChangePoints(startTime, endTime, slot_id, closed=False)
+                for point in changepoints:
+                    time = point["time"]
+                    cur = self.resDB.findAvailableSlots(time=time, slots=[slot_id])
+                    avail = cur.fetchone()["available"]
+                    capacity[slot_id].append((time,avail))
+                    
+                maxavail = float("inf")    
+                for cap in capacity[slot_id]:
+                    if cap[1] < maxavail:
+                        maxavail = cap[1]
+                        
+                if maxavail >= needed:
+                    candidateslots[slottype].append([slot[0],slot[1],maxavail])
+            
+            print "Slot type %i has candidates %s" % (slottype,candidateslots[slottype])
+        
+        # Make sure that available resources are all available on the same node
+        # (e.g. discard candidate nodes where we can provision memory but not the cpu)
+        nodeset = Set()
+        for slottype in slottypes:
+            nodes = Set([s[0] for s in candidateslots[slottype]])
+            nodeset |= nodes
+            
+        for slottype in slottypes:
+            nodes = Set([s[0] for s in candidateslots[slottype]])
+            nodeset &= nodes
+                
+        if len(nodeset) == 0:
+            print "This VW is unfeasible"
+            return
+        
+        # Keep only slots from nodeset
+        candidatenodes = {}
+        for slottype in slottypes:
+            for slot in candidateslots[slottype]:
+                nod_id = slot[0]
+                if nod_id in nodeset:
+                    if not candidatenodes.has_key(nod_id):
+                        candidatenodes[nod_id]={}
+                    candidatenodes[nod_id][slottype] = slot
+        for node in nodeset:    
+            print "Node %i has final candidates %s" % (node,candidatenodes[node])
+
+
+        # Decide if we can actually fit the entire VW
+        # This is the point were we can apply different slot fitting algorithms
+        # Currently, a greedy algorithm is used
+        # This is O(numvirtualnodes*numphysicalnodes), but could be implemented
+        # in O(numvirtualnodes). We'll worry about that later.
+        allfits = True
+        assignment = {}
+        for vwnode in range(0,numnodes):
+            assignment[vwnode] = {}
+            for physnode in nodeset:
+                fits = True
+                for slottype in slottypes:
+                    res = resources[slottype]
+                    if res > candidatenodes[physnode][slottype][2]:
+                        fits = False
+                if fits:
+                    for slottype in slottypes:
+                        res = resources[slottype]
+                        assignment[vwnode][slottype] = candidatenodes[physnode][slottype][1]
+                        candidatenodes[physnode][slottype][2] -= res
+                    break # Ouch
+            else:
+                print "Could not fit this virtual nodes in any physical node"
+                allfits = False
+                break
+            
+
+        if allfits:
+            print "This VW is feasible"
+            # Create reservation in db
+            res_id = self.resDB.addReservation("Test Reservation")
+            for vwnode in range(0,numnodes):
+                rsp_id = self.resDB.addReservationPart(res_id, "VM %i" % (vwnode+1), 0)
+                for slottype in slottypes:
+                    amount = resources[slottype]
+                    sl_id = assignment[vwnode][slottype]
+                    self.resDB.addSlot(rsp_id, sl_id, startTime, endTime, amount)
+        else:
+            print "This VW is unfeasible"
+            return
         
         # Is image transfer feasible?
 
