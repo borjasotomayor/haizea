718f37c43cc3a917487d451f1377ed3adf7237a7
Borja Sotomayor <borja@cs.uchicago.edu>
2006-12-11 00:32:14 +0000

    python.logging

--- python/workspace/ears/__init__.py
+++ python/workspace/ears/__init__.py
@@ -1,1 +1,10 @@
+import logging
 
+srvlog = logging.getLogger("server")
+reslog = logging.getLogger("reservations")
+
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+srvlog.addHandler(handler)
+srvlog.setLevel(logging.WARN)
