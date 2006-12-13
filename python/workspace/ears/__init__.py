ec0199164a537f1dc0d495b35ff2a49a018957d7
Borja Sotomayor <borja@cs.uchicago.edu>
2006-12-13 08:15:01 +0000

    Remove logging level hardcoding badness

--- python/workspace/ears/__init__.py
+++ python/workspace/ears/__init__.py
@@ -7,4 +7,13 @@ handler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 handler.setFormatter(formatter)
 srvlog.addHandler(handler)
-srvlog.setLevel(logging.WARN)
+
+loglevel = {"CRITICAL": 50,
+            "ERROR": 40,
+            "WARNING": 30,
+            "INFO": 20,
+            "SCHED": 15,
+            "DEBUG": 10,
+            "DEBUGSQL": 9,
+            "TRACE": 5,
+            "NOTSET": 0}
