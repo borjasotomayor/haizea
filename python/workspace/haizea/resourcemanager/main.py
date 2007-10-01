c5d9754e01f7f85ce9b22c1cd7bca2e3ded550dd
Borja Sotomayor <borja@cs.uchicago.edu>
2007-10-01 01:51:40 +0000

    Write statistics to disk

--- python/workspace/haizea/resourcemanager/main.py
+++ python/workspace/haizea/resourcemanager/main.py
@@ -6,6 +6,7 @@ import workspace.haizea.common.constants as constants
 import workspace.haizea.common.utils as utils
 from workspace.haizea.common.config import Config
 from workspace.haizea.common.log import log, loglevel
+from pickle import Pickler, Unpickler
 
 def simulate(configfile, tracefile, tracetype, injectedfile, statsdir):
     # Create config file
@@ -41,7 +42,32 @@ def writeDataToDisk(resourcemanager, dir):
     if not os.path.exists(dir):
         os.makedirs(dir)
         
+    cpuutilization = resourcemanager.stats.getUtilization(constants.RES_CPU)
+    cpuutilizationavg = resourcemanager.stats.getUtilizationAvg(constants.RES_CPU)
+    #memutilization = resourcemanager.stats.getUtilization(constants.RES_MEM)
+    #memutilizationavg = resourcemanager.stats.getUtilizationAvg(constants.RES_MEM)
+    exactaccepted = resourcemanager.stats.getExactAccepted()
+    exactrejected = resourcemanager.stats.getExactRejected()
+    besteffortcompleted = resourcemanager.stats.getBestEffortCompleted()
+    queuesize = resourcemanager.stats.getQueueSize()
+    queuewait = resourcemanager.stats.getQueueWait()
     
+    pickle(cpuutilization, dir, constants.CPUUTILFILE)
+    pickle(cpuutilizationavg, dir, constants.CPUUTILAVGFILE)
+    #pickle(memutilization, dir, constants.MEMUTILFILE)
+    #pickle(memutilizationavg, dir, constants.MEMUTILAVGFILE)
+    pickle(exactaccepted, dir, constants.ACCEPTEDFILE)
+    pickle(exactrejected, dir, constants.REJECTEDFILE)
+    pickle(besteffortcompleted, dir, constants.COMPLETEDFILE)
+    pickle(queuesize, dir, constants.QUEUESIZEFILE)
+    pickle(queuewait, dir, constants.QUEUEWAITFILE)
+    
+        
+def pickle(data, dir, file):
+    f = open (dir + "/" + file, "w")
+    p = Pickler(f)
+    p.dump(data)
+    f.close()
 
 
 if __name__ == "__main__":
