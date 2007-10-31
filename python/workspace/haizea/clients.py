72fea1ae0071be234355a82208bc069c9a7406d7
Borja Sotomayor <borja@cs.uchicago.edu>
2007-10-31 23:01:03 +0000

    Added command-line client to generate graphs with specific datasets, labels, and titles (instead of the ones automatically generated by the reporting program)

--- python/workspace/haizea/clients.py
+++ python/workspace/haizea/clients.py
@@ -2,9 +2,12 @@ import workspace.haizea.common.constants as constants
 from workspace.haizea.resourcemanager.main import simulate
 from workspace.haizea.traces.generators import generateTrace
 from workspace.haizea.common.utils import Option, OptionParser, generateScripts
-from workspace.haizea.common.config import RMConfig, RMMultiConfig, TraceConfig
+from workspace.haizea.common.config import RMConfig, RMMultiConfig, TraceConfig, GraphConfig
 from workspace.haizea.analysis.traces import analyzeExactLeaseInjection
 from workspace.haizea.analysis.misc import genpercentiles
+
+
+
 
 class Report(object):
     def __init__(self):
@@ -26,6 +29,41 @@ class Report(object):
         statsdir = opt.statsdir
 
         report(multiconfig, statsdir, opt.htmlonly)
+        
+class Graph(object):
+    def __init__(self):
+        pass
+    
+    def run(self, argv):
+        from workspace.haizea.analysis.report import Section
+        
+        p = OptionParser()
+        p.add_option(Option("-c", "--conf", action="store", type="string", dest="conf", required=True))
+        p.add_option(Option("-s", "--statsdir", action="store", type="string", dest="statsdir", required=True))
+
+        opt, args = p.parse_args(argv)
+        
+        configfile=opt.conf
+        graphconfig = GraphConfig.fromFile(configfile)
+            
+        statsdir = opt.statsdir
+
+        title = graphconfig.getTitle()
+        titlex = graphconfig.getTitleX()
+        titley = graphconfig.getTitleY()
+        datafile = graphconfig.getDatafile()
+        data = graphconfig.getDataEntries()
+        graphtype = graphconfig.getGraphType()
+
+        dirs = []
+        for d in data:
+            dirs.append((d.title, statsdir + "/" + d.dirname))
+        
+        s = Section(title, datafile, graphtype)
+        
+        s.loadData(dict(dirs), profilenames=[v[0] for v in dirs])
+        s.generateGraph("./", titlex=titlex, titley=titley)
+        
         
 class GenPercentiles(object):
     def __init__(self):
