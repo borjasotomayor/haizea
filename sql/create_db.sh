0687fc55a051198778649e430d390177d2c3a4c6
Borja Sotomayor <borja@cs.uchicago.edu>
2008-06-04 11:24:50 +0000

    Remove old SQL code (we don't swing that way anymore)

--- sql/create_db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm reservations.db
-sqlite3 reservations.db '.read create_db.sql'
-sqlite3 reservations.db '.read insert_types.sql'
-sqlite3 reservations.db '.read insert_testdata.sql'
--- sql/create_db_template.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm reservations_template.db
-sqlite3 reservations_template.db '.read create_db.sql'
-sqlite3 reservations_template.db '.read insert_types.sql'
