af030dc5367a52e71d6f67486fe38f64da1c660a
Borja Sotomayor <borja@cs.uchicago.edu>
2008-06-06 18:06:27 +0000

    Removed irrelevant Interface module (replaced by frontend modules)

--- src/haizea/resourcemanager/interface.py
+++
@@ -1,6 +0,0 @@
-class Interface(object):
-    def __init__(self, rm):
-        self.rm = rm
-        
-    # Interface class is otherwise empty. All events from this class
-    # are currently simulated.
