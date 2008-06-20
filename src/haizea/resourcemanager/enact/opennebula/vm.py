3e7966774efeb9bdf29419562755c660951d5edd
Borja Sotomayor <borja@cs.uchicago.edu>
2008-06-20 10:42:43 +0000

    OpenNebula suspend/resume enactment

--- src/haizea/resourcemanager/enact/opennebula/vm.py
+++ src/haizea/resourcemanager/enact/opennebula/vm.py
@@ -1,11 +1,16 @@
 from haizea.resourcemanager.enact.base import VMEnactmentBase
 import haizea.common.constants as constants
 import commands
+from pysqlite2 import dbapi2 as sqlite
 
 class VMEnactment(VMEnactmentBase):
     def __init__(self, resourcepool):
         VMEnactmentBase.__init__(self, resourcepool)
         self.onevm = "/home/borja/bin/onevm"
+        
+        self.conn = sqlite.connect(self.resourcepool.rm.config.getONEDB())
+        self.conn.row_factory = sqlite.Row
+
         
     def start(self, vms):
         print vms
@@ -50,8 +55,58 @@ class VMEnactment(VMEnactmentBase):
         if len(vms) > 1:
             raise Exception, "OpenNebula doesn't support more than 1 VM"
         vm = vms[0]
+        enactID = vm[3]
+        cmd = "%s suspend %i" % (self.onevm, enactID)
+        self.logger.debug("Running command: %s" % cmd, constants.ONE)
+        (status, output) = commands.getstatusoutput(cmd)
+        self.logger.debug("Returned status=%i, output='%s'" % (status, output), constants.ONE)
+        if status == 0:
+            self.logger.debug("Command returned succesfully.", constants.ONE)
+        else:
+            raise Exception, "Error when running onevm shutdown"
 
     def resume(self, vms):
         if len(vms) > 1:
             raise Exception, "OpenNebula doesn't support more than 1 VM"
         vm = vms[0]
+        enactID = vm[3]
+        cmd = "%s resume %i" % (self.onevm, enactID)
+        self.logger.debug("Running command: %s" % cmd, constants.ONE)
+        (status, output) = commands.getstatusoutput(cmd)
+        self.logger.debug("Returned status=%i, output='%s'" % (status, output), constants.ONE)
+        if status == 0:
+            self.logger.debug("Command returned succesfully.", constants.ONE)
+        else:
+            raise Exception, "Error when running onevm shutdown"
+
+    def verifySuspend(self, vms):
+        if len(vms) > 1:
+            raise Exception, "OpenNebula doesn't support more than 1 VM"
+        vm = vms[0]
+        enactID = vm[3]
+        cur = self.conn.cursor()
+        cur.execute("select state from vmpool where oid = %i" % enactID)
+        onevm = cur.fetchone()        
+        state = onevm["state"]
+        if state == 5:
+            self.logger.debug("Suspend correct.", constants.ONE)
+            return 0
+        else:
+            self.logger.warning("ONE did not complete suspend on time. State is %i" % state, constants.ONE)
+            return 1
+        
+    def verifyResume(self, vms):
+        if len(vms) > 1:
+            raise Exception, "OpenNebula doesn't support more than 1 VM"
+        vm = vms[0]
+        enactID = vm[3]
+        cur = self.conn.cursor()
+        cur.execute("select state from vmpool where oid = %i" % enactID)
+        vm = cur.fetchone()        
+        state = vm["state"]
+        if state == 3:
+            self.logger.debug("Resume correct.", constants.ONE)
+            return 0
+        else:
+            self.logger.warning("ONE did not complete resume on time. State is %i" % state, constants.ONE)
+            return 1
