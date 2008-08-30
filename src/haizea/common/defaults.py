3d4a3f76d874a74c6a04a32dd9c4b8056b35d8c6
Borja Sotomayor <borja@cs.uchicago.edu>
2008-08-30 19:45:54 +0000

    Added defaults module to contain default values, instead of hardcoding them throughout the code

---
+++ src/haizea/common/defaults.py
@@ -0,0 +1,11 @@
+import os.path
+
+
+CONFIG_LOCATIONS = ["/etc/haizea.conf",
+                            os.path.expanduser("~/.haizea/haizea.conf")]
+
+DAEMON_PIDFILE = "/var/tmp/haizea.pid"
+
+RPC_SERVER = "localhost"
+RPC_PORT = 42493
+RPC_URI = "http://%s:%i" % (RPC_SERVER, RPC_PORT)
