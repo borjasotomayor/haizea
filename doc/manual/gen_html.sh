64299a9cd269f1a759cf632fc928c87bcc3bb6cd
Borja Sotomayor <borja@cs.uchicago.edu>
2008-09-29 10:33:51 +0000

    Updated manual's HTML generation script to better partition the generated HTML.

--- doc/manual/gen_html.sh
+++ doc/manual/gen_html.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-latex2html -dir ../../html/manual/ -show_section_numbers -local_icons manual.tex 
+latex2html -split 4 -dir ../../html/manual/ -show_section_numbers -local_icons -toc_depth 3 -link 2 -top_navigation -bottom_navigation manual.tex
 tar cvzf ../../html/downloads/haizea-manual-multiple.tar.gz ../../html/manual
 
-latex2html -split 0 -no_navigation -dir ../../html/manual_single/ -show_section_numbers -local_icons manual.tex 
+latex2html -split 0 -no_navigation -dir ../../html/manual_single/ -show_section_numbers -local_icons -toc_depth 3 manual.tex 
 tar cvzf ../../html/downloads/haizea-manual-single.tar.gz ../../html/manual_single
 #tidy --clean y --doctype "transitional" --output-xhtml y --indent "auto" --wrap "90" --char-encoding "utf8" --logical-emphasis y
 
