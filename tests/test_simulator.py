93df5b058676ba745bb730d3bfa5d406bdda3292
Borja Sotomayor <borja@cs.uchicago.edu>
2009-01-30 00:07:03 +0000

    More tests

--- tests/test_simulator.py
+++ tests/test_simulator.py
@@ -21,3 +21,38 @@ def test_preemption_prematureend():
     config = load_configfile("base_config_simulator.conf", "preemption_prematureend.lwf")
     rm = ResourceManager(config)
     rm.start()
+    
+def test_preemption_prematureend2():
+    config = load_configfile("base_config_simulator.conf", "preemption_prematureend2.lwf")
+    rm = ResourceManager(config)
+    rm.start()
+    
+def test_reservation():
+    config = load_configfile("base_config_simulator.conf", "reservation.lwf")
+    rm = ResourceManager(config)
+    rm.start()
+    
+def test_reservation_prematureend():
+    config = load_configfile("base_config_simulator.conf", "reservation_prematureend.lwf")
+    rm = ResourceManager(config)
+    rm.start()
+    
+def test_migrate():
+    config = load_configfile("base_config_simulator.conf", "migrate.lwf")
+    rm = ResourceManager(config)
+    rm.start()
+    
+def test_reuse1():
+    config = load_configfile("base_config_simulator.conf", "reuse1.lwf")
+    rm = ResourceManager(config)
+    rm.start()
+    
+def test_reuse2():
+    config = load_configfile("base_config_simulator.conf", "reuse2.lwf")
+    rm = ResourceManager(config)
+    rm.start()
+    
+def test_wait():
+    config = load_configfile("base_config_simulator.conf", "wait.lwf")
+    rm = ResourceManager(config)
+    rm.start()
