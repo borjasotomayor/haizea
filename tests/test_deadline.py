44c4e6d4a9b7fb5c4615bb9ff3dae875267c4901
Borja Sotomayor <borja@cs.uchicago.edu>
2010-06-22 17:04:45 +0000

    - Changed unit tests to use test_* functions instead of Test* classes. Newer py.test is apparently not too happy about test classes having a constructor. - Minor changes.

--- tests/test_deadline.py
+++ tests/test_deadline.py
@@ -1,125 +1,206 @@
-from common import BaseTest
 from haizea.core.leases import Lease
+from common import *
 
-class TestSimulator(BaseTest):
-    def __init__(self):
-        config = BaseTest.load_configfile("base_config_simulator.conf")
-        BaseTest.__init__(self, config)
-        self.config.set("scheduling", "mapper", "deadline")
-        self.config.set("scheduling", "policy-preemption", "deadline")
-        self.config.set("scheduling", "suspension", "all")
+def get_config():
+    c = load_configfile("base_config_simulator.conf")
+    c.set("scheduling", "policy-preemption", "deadline")
+    c.set("scheduling", "suspension", "all")
+    return c
+
+def test_deadline1():
+    c = get_config()
+    h = load_tracefile(c, "deadline1.lwf")
+    h.start()    
+    verify_done(h, [1,2])          
+    
+def test_deadline2():
+    c = get_config()
+    h = load_tracefile(c, "deadline2.lwf")
+    h.start()
+    verify_done(h, [1,2])          
+    
+def test_deadline3():
+    c = get_config()
+    h = load_tracefile(c, "deadline3.lwf")
+    h.start()
+    verify_done(h, [1,2])          
+    
+def test_deadline4():
+    c = get_config()
+    h = load_tracefile(c, "deadline4.lwf")
+    h.start()
+    verify_done(h, [1])          
+    verify_rejected(h, [2])          
             
-    def test_deadline1(self):
-        self._tracefile_test("deadline1.lwf")
-        self._verify_done([1,2])          
-        
-    def test_deadline2(self):
-        self._tracefile_test("deadline2.lwf")
-        self._verify_done([1,2])          
-        
-    def test_deadline3(self):
-        self._tracefile_test("deadline3.lwf")
-        self._verify_done([1,2])          
-        
-    def test_deadline4(self):
-        self._tracefile_test("deadline4.lwf")
-        self._verify_done([1])          
-        self._verify_rejected([2])          
-                
-    def test_deadline5(self):
-        self._tracefile_test("deadline5.lwf")
-        self._verify_done([1,2])          
+def test_deadline5():
+    c = get_config()
+    h = load_tracefile(c, "deadline5.lwf")
+    h.start()
+    verify_done(h, [1,2])          
 
-    def test_deadline6(self):
-        self._tracefile_test("deadline6.lwf")
-        self._verify_done([1,2,3])          
-        
-    def test_deadline7(self):
-        self._tracefile_test("deadline7.lwf")
-        self._verify_done([1,2,3])          
-        
-    def test_deadline8(self):
-        self._tracefile_test("deadline8.lwf")
-        self._verify_done([1,2])          
-        self._verify_rejected([3])
-                                
-    def test_deadline9(self):
-        self._tracefile_test("deadline9.lwf")
-        self._verify_done([1,2,3])
-        
-    def test_deadline10_1(self):
-        self._tracefile_test("deadline10-1.lwf")
-        self._verify_done([1,2,3])
+def test_deadline6():
+    c = get_config()
+    h = load_tracefile(c, "deadline6.lwf")
+    h.start()
+    verify_done(h, [1,2,3])          
+    
+def test_deadline7():
+    c = get_config()
+    h = load_tracefile(c, "deadline7.lwf")
+    h.start()
+    verify_done(h, [1,2,3])          
+    
+def test_deadline8():
+    c = get_config()
+    h = load_tracefile(c, "deadline8.lwf")
+    h.start()
+    verify_done(h, [1,2])          
+    verify_rejected(h, [3])
+                            
+def test_deadline9_1():
+    c = get_config()
+    h = load_tracefile(c, "deadline9.lwf")
+    h.start()
+    verify_done(h, [1,2,3])
+    
+def test_deadline9_2():
+    c = get_config()
+    c.set("scheduling", "suspension", "none")     
+    h = load_tracefile(c, "deadline9.lwf")
+    h.start()       
+    verify_done(h, [1,2,3])      
+    
+def test_deadline10_1():
+    c = get_config()
+    c.set("scheduling", "suspension", "none")     
+    h = load_tracefile(c, "deadline10-1.lwf")
+    h.start()       
+    verify_done(h, [1,2,3])
 
-    def test_deadline10_2(self):
-        self._tracefile_test("deadline10-2.lwf")
-        self._verify_done([1,2,3])
+def test_deadline10_2():
+    c = get_config()
+    c.set("scheduling", "suspension", "none")     
+    h = load_tracefile(c, "deadline10-2.lwf")
+    h.start()       
+    verify_done(h, [1,2,3])
 
-    def test_deadline10_3(self):
-        self._tracefile_test("deadline10-3.lwf")
-        self._verify_done([1,2,3])
+def test_deadline10_3():
+    c = get_config()
+    c.set("scheduling", "suspension", "none")     
+    h = load_tracefile(c, "deadline10-3.lwf")
+    h.start()       
+    verify_done(h, [1,2,3])
 
-    def test_deadline10_4(self):
-        self._tracefile_test("deadline10-4.lwf")
-        self._verify_done([1,2,3])
+def test_deadline10_4():
+    c = get_config()
+    c.set("scheduling", "suspension", "none")     
+    h = load_tracefile(c, "deadline10-4.lwf")
+    h.start()       
+    verify_done(h, [1,2,3])
 
-    def test_deadline10_5(self):
-        self._tracefile_test("deadline10-5.lwf")
-        self._verify_done([1,2,3])
+def test_deadline10_5():
+    c = get_config()
+    c.set("scheduling", "suspension", "none")     
+    h = load_tracefile(c, "deadline10-5.lwf")
+    h.start()       
+    verify_done(h, [1,2,3])
 
-    def test_deadline10_6(self):
-        self._tracefile_test("deadline10-6.lwf")
-        self._verify_done([1,2,3])
+def test_deadline10_6():
+    c = get_config()
+    c.set("scheduling", "suspension", "none")     
+    h = load_tracefile(c, "deadline10-6.lwf")
+    h.start()       
+    verify_done(h, [1,2,3])
 
-    def test_deadline10_7(self):
-        self._tracefile_test("deadline10-7.lwf")
-        self._verify_done([1,2,3])
+def test_deadline10_7():
+    c = get_config()
+    c.set("scheduling", "suspension", "none")     
+    h = load_tracefile(c, "deadline10-7.lwf")
+    h.start()       
+    verify_done(h, [1,2,3])
 
-    def test_deadline10_8(self):
-        self._tracefile_test("deadline10-8.lwf")
-        self._verify_done([1,2,3])
+def test_deadline10_8():
+    c = get_config()
+    c.set("scheduling", "suspension", "none")     
+    h = load_tracefile(c, "deadline10-8.lwf")
+    h.start()       
+    verify_done(h, [1,2,3])
 
-    def test_deadline10_9(self):
-        self._tracefile_test("deadline10-9.lwf")
-        self._verify_done([1,2,3])
+def test_deadline10_9():
+    c = get_config()
+    c.set("scheduling", "suspension", "none")     
+    h = load_tracefile(c, "deadline10-9.lwf")
+    h.start()       
+    verify_done(h, [1,2,3])
 
-    def test_deadline10_10(self):
-        self._tracefile_test("deadline10-10.lwf")
-        self._verify_done([1,2,3])
+def test_deadline10_10():
+    c = get_config()
+    c.set("scheduling", "suspension", "none")     
+    h = load_tracefile(c, "deadline10-10.lwf")
+    h.start()       
+    verify_done([1,2,3])
 
-    def test_deadline10_11(self):
-        self._tracefile_test("deadline10-11.lwf")
-        self._verify_done([1,2,3])
+def test_deadline10_11():
+    c = get_config()
+    c.set("scheduling", "suspension", "none")     
+    h = load_tracefile(c, "deadline10-11.lwf")
+    h.start()       
+    verify_done(h, [1,2,3])
 
-    def test_deadline10_12(self):
-        self._tracefile_test("deadline10-12.lwf")
-        self._verify_done([1,2,3])
+def test_deadline10_12():
+    c = get_config()
+    c.set("scheduling", "suspension", "none")     
+    h = load_tracefile(c, "deadline10-12.lwf")
+    h.start()       
+    verify_done(h, [1,2,3])
 
-    def test_deadline11_1(self):
-        self._tracefile_test("deadline11-1.lwf")
-        self._verify_done([1,2,3])
+def test_deadline11_1():
+    c = get_config()
+    c.set("scheduling", "suspension", "none")     
+    h = load_tracefile(c, "deadline11-1.lwf")
+    h.start()       
+    verify_done(h, [1,2,3])
 
-    def test_deadline11_2(self):
-        self._tracefile_test("deadline11-2.lwf")
-        self._verify_done([1,2,3])
+def test_deadline11_2():
+    c = get_config()
+    c.set("scheduling", "suspension", "none")     
+    h = load_tracefile(c, "deadline11-2.lwf")
+    h.start()       
+    verify_done(h, [1,2,3])
 
-    def test_deadline12(self):
-        self._tracefile_test("deadline12.lwf")
-        self._verify_done([1,2,3,4,5,6,7,8])
-        
-    def test_deadline13(self):
-        self._tracefile_test("deadline13.lwf")
-        self._verify_done([1,2,3])        
-        
-    def test_deadline14(self):
-        self._tracefile_test("deadline14.lwf")
-        self._verify_done([1,2,3])            
+def test_deadline12():
+    c = get_config()
+    c.set("scheduling", "suspension", "none")     
+    h = load_tracefile(c, "deadline12.lwf")
+    h.start()       
+    verify_done(h, [1,2,3,4,5,6,7,8])
+    
+def test_deadline13():
+    c = get_config()
+    c.set("scheduling", "suspension", "none")     
+    h = load_tracefile(c, "deadline13.lwf")
+    h.start()       
+    verify_done(h, [1,2,3])        
+    
+def test_deadline14():
+    c = get_config()
+    c.set("scheduling", "suspension", "none")     
+    h = load_tracefile(c, "deadline14.lwf")
+    h.start()       
+    verify_done(h, [1,2,3])            
 
-    def test_deadline15(self):
-        self._tracefile_test("deadline15.lwf")
-        self._verify_done([1,2,3])            
-        self._verify_rejected([4])
+def test_deadline15():
+    c = get_config()
+    c.set("scheduling", "suspension", "none")     
+    h = load_tracefile(c, "deadline15.lwf")
+    h.start()       
+    verify_done(h, [1,2,3])            
+    verify_rejected(h, [4])
 
-    def test_deadline16(self):
-        self._tracefile_test("deadline16.lwf")
-        self._verify_done([1,2,3,4,5,6])
+def test_deadline16():
+    c = get_config()
+    c.set("scheduling", "suspension", "none")     
+    h = load_tracefile(c, "deadline16.lwf")
+    h.start()
+    verify_done(h, [1,2,3,4,5,6])
+      
--- tests/test_simul_pricing.py
+++ tests/test_simul_pricing.py
@@ -1,34 +1,40 @@
-from common import BaseTest
 from haizea.core.leases import Lease
+from common import *
 
-class TestSimulator(BaseTest):
-    def __init__(self):
-        config = BaseTest.load_configfile("base_config_simulator.conf")
-        BaseTest.__init__(self, config)
-        self.config.add_section("pricing")
-            
-    def test_pricing1(self):
-        self.config.set("scheduling", "policy-pricing", "free")
-        self._tracefile_test("price1.lwf")
-        self._verify_done([1])
+def get_config():
+    c = load_configfile("base_config_simulator.conf")
+    c.add_section("pricing")
+    return c
+   
+def test_pricing1():
+    c = get_config()
+    c.set("scheduling", "policy-pricing", "free")
+    h = load_tracefile(c, "price1.lwf")
+    h.start()
+    verify_done(h, [1])
 
-    def test_pricing2(self):
-        self.config.set("scheduling", "policy-pricing", "constant")
-        self.config.set("pricing", "rate", "0.10")        
-        self._tracefile_test("price1.lwf")
-        self._verify_done([1])
-        
-    def test_pricing3(self):
-        self.config.set("scheduling", "policy-pricing", "constant")
-        self.config.set("pricing", "rate", "1.00")        
-        self._tracefile_test("price2.lwf")
-        self._verify_rejected_by_user([1])
-        
-    def test_pricing_surcharge(self):
-        self.config.set("scheduling", "mapper", "deadline")
-        self.config.set("scheduling", "policy-preemption", "deadline")
-        self.config.set("scheduling", "suspension", "all")
-        self.config.set("scheduling", "policy-pricing", "constant")
-        self.config.set("pricing", "rate", "0.10")        
-        self._tracefile_test("pricedeadline.lwf")
-        self._verify_done([1])        
+def test_pricing2():
+    c = get_config()
+    c.set("scheduling", "policy-pricing", "constant")
+    c.set("pricing", "rate", "0.10")        
+    h = load_tracefile(c, "price1.lwf")
+    h.start()
+    verify_done(h, [1])
+    
+def test_pricing3():
+    c = get_config()
+    c.set("scheduling", "policy-pricing", "constant")
+    c.set("pricing", "rate", "1.00")        
+    h = load_tracefile(c, "price2.lwf")
+    h.start()
+    verify_rejected_by_user(h, [1])
+    
+def test_pricing_surcharge():
+    c = get_config()
+    c.set("scheduling", "policy-preemption", "deadline")
+    c.set("scheduling", "suspension", "all")
+    c.set("scheduling", "policy-pricing", "constant")
+    c.set("pricing", "rate", "0.10")        
+    h = load_tracefile(c, "pricedeadline.lwf")
+    h.start()
+    verify_done(h, [1])        
